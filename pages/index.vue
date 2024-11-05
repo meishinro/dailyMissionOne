c5400e51e68c196d6adbdc9536fd7c246fd190e4
Ming Chen Lu <mingchenlu@MingdeMacBook-Air.local>
2024-11-05 23:50:10 +0900

    create homePage

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <h1>
+    Page: 首頁
+  </h1>
+  <p>
+    目前路由的路徑 - route.fullpath 為 : {{ route.fullPath }}
+  </p>
+</template>
+
+<style scoped></style>
